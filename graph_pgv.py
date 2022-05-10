5a4424f7c5dccbbc59863631dff9613aa6c1f681
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-10 11:50:08 -0700

    Added more pokemon to viz

--- graph_pgv.py
+++ graph_pgv.py
@@ -3,11 +3,13 @@ import pygraphviz as pgv
 
 G = pgv.AGraph(strict=False, directed=True)
 
-name = "charmander"
-G.add_node(0, label=name[0])
-for i in range(1, len(name)):
-  G.add_node(i, label=name[i])
-  G.add_edge(i-1, i)
+names = ["charmander", "charizard"]
+for name in names:
+  if G.has_node(name[0]):
+    G.add_node(name[0], label=name[0])
+  for i in range(1, len(name)):
+    G.add_node(i, label=name[i])
+    G.add_edge(i-1, i)
 
 G = G.acyclic(copy=True)
 
