1c91fe59e340c276dd69e07a9cf1cb264d909491
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2022-05-09 18:49:06 -0700

    idk

--- graph_vis.py
+++ graph_vis.py
@@ -6,19 +6,25 @@ import matplotlib.pyplot as plt
 
 g = nx.Graph()
 
-g.add_edge(1, 2)
-g.add_edge(2, 3)
-g.add_edge(3, 4)
-g.add_edge(1, 4)
-g.add_edge(1, 5)
-g.add_edge(5, 6)
-g.add_edge(5, 7)
-g.add_edge(4, 8)
-g.add_edge(3, 8)
+# g.add_edge(1, 2)
+# g.add_edge(2, 3)
+# g.add_edge(3, 4)
+# g.add_edge(1, 4)
+# g.add_edge(1, 5)
+# g.add_edge(5, 6)
+# g.add_edge(5, 7)
+# g.add_edge(4, 8)
+# g.add_edge(3, 8)
+
+g.add_edge('c', 'h')
+g.add_edge('h', 'a')
+g.add_edge('a', 'r')
+g.add_edge('r', 'm')
+g.add_edge('m', 'a')
+g.add_edge('a', 'n')
 
 
 # drawing in spectral layout
-nx.draw_spectral(g, with_labels = True)
-plt.savefig("filename4.png")
-
-
+nx.draw(g, with_labels=True)
+# plt.savefig("filename4.png")
+plt.savefig("test1.png")
