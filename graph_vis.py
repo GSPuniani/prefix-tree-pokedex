26065e3898b0c22ca98296a3562ca57126d1da20
Gobind Puniani <gobindpuniani@gmail.com>
2022-05-09 18:16:39 -0700

    Added networkx and matplotlib graph visualization code

---
+++ graph_vis.py
@@ -0,0 +1,24 @@
+# First networkx library is imported
+# along with matplotlib
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+g = nx.Graph()
+
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+g.add_edge(1, 4)
+g.add_edge(1, 5)
+g.add_edge(5, 6)
+g.add_edge(5, 7)
+g.add_edge(4, 8)
+g.add_edge(3, 8)
+
+
+# drawing in spectral layout
+nx.draw_spectral(g, with_labels = True)
+plt.savefig("filename4.png")
+
+
