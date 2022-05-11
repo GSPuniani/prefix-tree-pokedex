53914d6181c8fe1cee7b72ba988973ed4217f88f
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2022-05-10 19:10:55 -0700

    edited

--- graph_pgv.py
+++ graph_pgv.py
@@ -5,11 +5,11 @@ G = pgv.AGraph(strict=False, directed=True)
 
 names = ["charmander", "charizard"]
 for name in names:
-  if G.has_node(name[0]):
-    G.add_node(name[0], label=name[0])
-  for i in range(1, len(name)):
-    G.add_node(i, label=name[i])
-    G.add_edge(i-1, i)
+    if G.has_node(name[0]):
+        G.add_node(name[0], label=name[0])
+    for i in range(1, len(name)):
+        G.add_node(i, label=name[i])
+        G.add_edge(i-1, i)
 
 G = G.acyclic(copy=True)
 
